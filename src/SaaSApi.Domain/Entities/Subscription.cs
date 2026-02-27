396cb3187f2bd23a737bd2f53a6069bbaa2b2f83
Mauro F. <maforeror@gmail.com>
2026-02-27 15:44:33 -0500

    feat: creating Subscription entity and some config files

---
+++ src/SaaSApi.Domain/Entities/Subscription.cs
@@ -0,0 +1,29 @@
+﻿namespace SaaSApi.Domain;
+
+public class Subscription
+{
+    public Guid Id { get; private set; }
+    public string UserEmail { get; private set; }
+    public string PlanName { get; private set; }
+    public DateTime EndDate { get; private set; }
+    public bool IsActivate { get; private set; }
+
+    public Subscription(
+        Guid id,
+        string email,
+        string planName,
+        int durationMonths
+    )
+    {
+        if(string.IsNullOrEmpty(email) || !email.Contains("@"))
+            throw new ArgumentException("This email is invalid.");
+
+        Id = id;
+        UserEmail = email;
+        PlanName = planName;
+        EndDate = DateTime.UtcNow.AddMonths(durationMonths);
+        IsActivate = true;
+    }
+
+    public void Cancel() => IsActivate = false;
+}
