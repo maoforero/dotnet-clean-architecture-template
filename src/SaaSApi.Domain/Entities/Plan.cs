96c0c9ba0fd2199df3bcd72590b6247b8b7d18c3
Mauro F. <maforeror@gmail.com>
2026-03-04 22:52:26 -0500

    new plan entity

--- src/SaaSApi.Domain/Entities/Plan.cs
+++ src/SaaSApi.Domain/Entities/Plan.cs
@@ -1,10 +1,4 @@
 public class Plan
 {
-    public string Name { get; private set; }
-    public string Description { get; private set; }
-    public decimal Price { get; private set; }
-    public BillingCycle BillingCycle { get; private set; }
-    public bool Status { get; private set; }
-
     
 }
