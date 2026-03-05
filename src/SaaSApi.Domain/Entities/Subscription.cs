dd1880c21fb41c5b23304e7d3f6ffb2e38760342
Mauro F. <maforeror@gmail.com>
2026-03-04 20:06:52 -0500

    new properties in subscription entity

--- src/SaaSApi.Domain/Entities/Subscription.cs
+++ src/SaaSApi.Domain/Entities/Subscription.cs
@@ -3,27 +3,9 @@
 public class Subscription : BaseEntity
 {
     public Guid Id { get; private set; }
-    public string UserEmail { get; private set; }
-    public string PlanName { get; private set; }
+    public Guid PlanId { get; private set; }
+    public SubscriptionStatus Status { get; private set; }
+    public BillingCycle BillingCycle { get; private set; }
+    public DateTime StartDate { get; private set; }
     public DateTime EndDate { get; private set; }
-    public bool IsActivate { get; private set; }
-
-    public Subscription(
-        Guid id,
-        string email,
-        string planName,
-        int durationMonths
-    )
-    {
-        if(string.IsNullOrEmpty(email) || !email.Contains("@"))
-            throw new ArgumentException("This email is invalid.");
-
-        Id = id;
-        UserEmail = email;
-        PlanName = planName;
-        EndDate = DateTime.UtcNow.AddMonths(durationMonths);
-        IsActivate = true;
-    }
-
-    public void Cancel() => IsActivate = false;
 }
