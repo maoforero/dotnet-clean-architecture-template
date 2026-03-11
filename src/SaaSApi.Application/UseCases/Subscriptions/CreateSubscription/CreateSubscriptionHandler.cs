202b6c2be778301476d506d57ce961357abb1020
Mauro F. <maforeror@gmail.com>
2026-03-10 22:19:56 -0500

    handler template

--- src/SaaSApi.Application/UseCases/Subscriptions/CreateSubscription/CreateSubscriptionHandler.cs
+++ src/SaaSApi.Application/UseCases/Subscriptions/CreateSubscription/CreateSubscriptionHandler.cs
@@ -1,4 +1,23 @@
 public class CreateSubscriptionHandler
 {
-    
+    private readonly IPlanRepository _planRepository;
+    private readonly ISubscriptionRepository _subcriptionRepository;
+    private readonly IUserRepository _userRepository;
+
+    public CreateSubscriptionHandler
+    (
+        IPlanRepository planRepository,
+        ISubscriptionRepository subscriptionRepository,
+        IUserRepository userRepository
+    )
+    {
+        _planRepository = planRepository;
+        _subcriptionRepository = subscriptionRepository;
+        _userRepository = userRepository;    
+    }
+
+    public Task<CreateSubscriptionResult> HandleAsync(CreateSubscriptionCommand command)
+    {
+
+    }
 }
