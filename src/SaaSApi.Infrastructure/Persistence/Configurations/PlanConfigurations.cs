2b9a8327fbcf65db988ce0d179f52e01e144d758
Mauro F. <maforeror@gmail.com>
2026-03-13 23:50:55 -0500

    fixinc PlanConfiguration

--- src/SaaSApi.Infrastructure/Persistence/Configurations/PlanConfigurations.cs
+++ src/SaaSApi.Infrastructure/Persistence/Configurations/PlanConfigurations.cs
@@ -5,6 +5,27 @@ public class PlanConfigurations : IEntityTypeConfiguration<Plan>
 {
     public void Configure(EntityTypeBuilder<Plan> builder)
     {
-        throw new NotImplementedException();
+        builder.ToTable("plansgit a");
+
+        builder.Property(p => p.Name)
+            .HasMaxLength(100)
+            .HasColumnName("name")
+            .IsRequired();
+
+        builder.Property(p => p.Description)
+            .HasMaxLength(256)
+            .HasColumnName("description");
+
+        builder.Property(p => p.Price)
+            .HasColumnName("price")
+            .IsRequired();
+
+        builder.Property(p => p.BillingCycle)
+            .HasColumnName("billing_cycle")
+            .IsRequired();
+
+        builder.Property(p => p.IsActive)
+            .HasColumnName("is_active")
+            .IsRequired();
     }
 }
