a300d4366009e33cc57311a395ad6b8bb306300b
Mauro F. <maforeror@gmail.com>
2026-03-13 23:45:08 -0500

    UserConfigurationComplete

--- src/SaaSApi.Infrastructure/Persistence/Configurations/UserConfigurations.cs
+++ src/SaaSApi.Infrastructure/Persistence/Configurations/UserConfigurations.cs
@@ -6,6 +6,30 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
 {
     public void Configure(EntityTypeBuilder<User> builder)
     {
-        throw new NotImplementedException();
+        builder.ToTable("users");
+
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Id)
+            .HasColumnName("id");
+
+        builder.Property(c =>  c.FirstName)
+            .HasColumnName("first_name")
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(c => c.LastName)
+            .HasColumnName("last_name")
+            .HasMaxLength(100)
+            .IsRequired();
+        
+        builder.Property(c => c.Email)
+            .HasColumnName("email")
+            .HasMaxLength(256)
+            .IsRequired();
+        
+        builder.HasIndex(u => u.Email).IsUnique();
+
+        builder.Property(u => u.ActiveSubscriptionId)
+            .HasColumnName("active_subscription_id");
     }
 }
