f013c98cc9720fe3d37dac7e5b4752a5c2ea23b6
Mauro F. <maforeror@gmail.com>
2026-03-27 16:45:10 -0500

    adding middleare exceptions

---
+++ src/SaaSApi.Api/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,21 @@
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            
+        }
+    }
+}
