93a199eee86e2fa7f3edc706dddb00249da0ae6f
Mauro F. <maforeror@gmail.com>
2026-03-29 23:36:44 -0500

    adding new cancel subs handler

--- src/SaaSApi.Application/UseCases/Subscriptions/CancelSubscription/CancelSubscriptionHandler.cs
+++ src/SaaSApi.Application/UseCases/Subscriptions/CancelSubscription/CancelSubscriptionHandler.cs
@@ -1,18 +1,17 @@
+using SaaSApi.Domain;
+
 public class CancelSubscriptionHandler
 {
-    private readonly IPlanRepository _planRepository;
     private readonly ISubscriptionRepository _subscriptionrepository;
     private readonly IUserRepository _userRepository;
     private readonly IUnitOfWork _unitOfWork;
 
     public CancelSubscriptionHandler(
-        IPlanRepository planRepository,
         ISubscriptionRepository subscriptionRepository,
         IUserRepository userRepository,
         IUnitOfWork unitOfWork
     )
     {
-        _planRepository = planRepository;
         _subscriptionrepository = subscriptionRepository;
         _userRepository = userRepository;
         _unitOfWork = unitOfWork;
@@ -20,6 +19,22 @@ public class CancelSubscriptionHandler
 
     public async Task<CancelSubscriptionResult> HandleAsync(CancelSubscriptionCommand command, CancellationToken ct)
     {
-        var userId = await _userRepository.GetByIdAsync(command.PlanId)
+        var userId = await _userRepository.GetByIdAsync(command.UserId, ct);
+        if(userId == null) throw new ArgumentException("User Id doesnt exist");
+
+        var subscription = await _subscriptionrepository.GetActiveByUserIdAsync(command.UserId, ct);
+        if(subscription == null) throw new ArgumentException("Subscription doesnt exist");
+
+        subscription.Cancel();
+
+        await _subscriptionrepository.UpdateAsync(subscription, ct);
+        await _unitOfWork.CommitAsync(ct);
+
+        return new CancelSubscriptionResult
+        {
+            SubscriptionId = subscription.Id,
+            Status = subscription.Status,
+            EndDate = subscription.EndDate
+        };
     }
 }
